9430aa9f3229b389ee85577215478cbbc895c835
Tato Levicz <leviczios@gmail.com>
2024-09-19 21:01:45 -0300

    refactor cmake

---
+++ cmake/mgutils.cmake
@@ -0,0 +1,15 @@
+set (MGUTILS_HEADERS
+    ${CMAKE_CURRENT_SOURCE_DIR}/include/mgutils/mgutils
+)
+
+set (MGUTILS_SRCS
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/Json.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/Logger.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/Logger.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/json/JsonDocument.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/json/JsonValue.cpp
+)
+
+set (MGUTILS_INCLUDE_DIRS
+    ${CMAKE_CURRENT_SOURCE_DIR}/include
+)
